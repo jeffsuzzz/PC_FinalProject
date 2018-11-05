309b5009776846ccfa58b0dd4c70da5e59ac59e6
jeffsuzzz <jeffsuzzz@gmail.com>
2018-11-05 14:41:41 -0500

    Add getKey method

--- Sim_key.java
+++ Sim_key.java
@@ -7,13 +7,26 @@ public class Sim_key {
 	int movieId2;
 	
 	public Sim_key(int k1, int k2) {
-		this.movieId1 = k1;
-		this.movieId2 = k2;
+		if(k1 < k2) {
+			this.movieId1 = k1;
+			this.movieId2 = k2;
+		} else {
+			this.movieId1 = k2;
+			this.movieId2 = k1;
+		}
+	}
+	
+	public int getKey1() {
+		return this.movieId1;
+	}
+	
+	public int getKey2() {
+		return this.movieId2;
 	}
 	
 	@Override
 	public int hashCode() {
-		return movieId1 + movieId2;
+		return Integer.hashCode(movieId1) + Integer.hashCode(movieId2);
 	}
 	
 	@Override
